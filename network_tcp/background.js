c810a96b5a862bdbc5c998761f6819d69bcf417a
xjr7670 <xjr30226@126.com>
2018-12-07 21:31:51 +0800

    TCP示例，不是我写的

---
+++ network_tcp/background.js
@@ -0,0 +1,66 @@
+var topServerSocket = new tcpServer();
+tcpServerSocket.option = {
+    persistent: true
+};
+
+tcpServerSocket.accept = handleAccept.bind(tcpServerSocket);
+tcpServerSocket.init(function() {
+    tcpServerSocket.listen('127.0.0.1', 80, function() {
+        console.log('Listening 127.0.0.1:80...');
+    });
+});
+
+function handleAccept(info) {
+    if (info.socketId == this.socketId) {
+        var _tcp = chrome.sockets.tcp;
+        var tcpSocket = new tcp();
+        tcpSocket.socketId = info.clientSocketId;
+        tcpSocket.keetAlive(true, 5, function() {
+            _tcp.onReceive.addListener(function(info) {
+                if (info.socketId == tcpSocket.socketId) {
+                    tcpSocket.receive(info);
+                }
+            });
+            _tcp.onReceiveError.addListener(function(info) {
+                if (info.socketId == tcpSocket.socketId) {
+                    tcpSocket.error(info.resultCode);
+                }
+            });
+            tcpSocket.receive = handleRequest.bind(tcpSocket);
+            tcpSocket.pause(false, function() {
+                console.log('Receiving data...');
+            });
+        });
+    }
+}
+
+function handleRequest(info) {
+    var header = ab2str(info.data);
+    header = header.split("\r\n").join('<br />');
+    var body = "<h1>It Works!</h1>" + "<hr />" + "Request Header: <br />" + header;
+    var response = "HTTP/1.1 200 OK\r\n" +
+                   "Connection: Keep-Alive\r\n" +
+                   "Content-Length: " + body.length + "\r\n" +
+                   "Content-Type: text/html\r\n" +
+                   "Connection: close\r\n\r\n" + body;
+    response = str2ab(response);
+    this.send(response, function() {
+        console.log('Sent.');
+        this.close(function() {
+            console.log('Closed.');
+        })
+    }.bind(this));
+}
+
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length);
+    bufView = new Unit8Array(buf);
+    for (var i=0; i < str.length; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
+function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
